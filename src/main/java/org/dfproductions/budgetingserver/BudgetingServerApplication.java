553dab18fd15543d387a422f01a952a78c602088
A.P. <eternalsuffering101@gmail.com>
2024-09-28 03:49:16 +0300

    Initial commit. BudgetingServer v0.0.1

---
+++ src/main/java/org/dfproductions/budgetingserver/BudgetingServerApplication.java
@@ -0,0 +1,13 @@
+package org.dfproductions.budgetingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetingServerApplication.class, args);
+	}
+
+}
